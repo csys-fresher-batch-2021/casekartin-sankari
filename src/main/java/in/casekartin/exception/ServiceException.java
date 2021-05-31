9a555ad85f67851e26f89af4edcbee68da999e7e
selva08sankari <sankari08kujo@gmail.com>
2021-05-31 17:25:56 +0530

    bug fixed

--- src/main/java/in/casekartin/exception/ServiceException.java
+++ src/main/java/in/casekartin/exception/ServiceException.java
@@ -2,10 +2,6 @@ package in.casekartin.exception;
 
 public class ServiceException extends Exception {
 
-	public ServiceException(String message, NumberException e) {
-		super(message,e);
-	}
-
 	public ServiceException(String message, Exception e) {
 		super(message,e);
 	}
